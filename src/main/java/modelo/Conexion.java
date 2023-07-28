714feeaecc04773046e594b963f599345b0fc681
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-07-28 00:44:06 -0300

    Clase Conexion y clase Usuario.
    La clase Conexion se usa para conectarse a una base de datos MySQL.
    
    La clase Usuario se usa para mapear los usuarios de la tabla "usuario"
    de la BBDD.

---
+++ src/main/java/modelo/Conexion.java
@@ -0,0 +1,30 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+	private Connection conexion;
+	private final String URL = "jdbc:mysql://localhost:3306/registro-jsp";
+	private final String USER = "root";
+	private final String PASSWORD = "root";
+
+	public void conectar() throws Exception {
+		try {
+			conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public void desconectar() throws SQLException {
+		if (conexion != null && !conexion.isClosed())
+			conexion.close();
+	}
+
+	public Connection getConexion() {
+		return this.conexion;
+	}
+}
---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,69 @@
+package modelo;
+
+public class Usuario {
+
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String usuario;
+	private String contrasena;
+	private String pais;
+	private String tecnologia;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+
+	public String getTecnologia() {
+		return tecnologia;
+	}
+
+	public void setTecnologia(String tecnologia) {
+		this.tecnologia = tecnologia;
+	}
+
+}
