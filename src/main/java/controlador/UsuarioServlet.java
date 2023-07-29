b57d357ff3fe3da1dbb7369e42b314497f6571e4
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-07-28 23:17:34 -0300

    Target server y creación de servlet controlador.
    Se indicó que el target server es Tomcat.
    
    Se creó el servlet UsuarioServlet que actúa como controlador para
    usuarios, y se sobreescribió el método "doPost" para agregar los nuevos
    usuarios en la BBDD.
    
    NOTAR: en el servlet se especificó la codificación de caracteres del request, y se usa response.getWriter() para escribir en la página.

---
+++ src/main/java/controlador/UsuarioServlet.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import modelo.Usuario;
+import modelo.UsuarioDAO;
+import modelo.UsuarioDAOImpl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UsuarioServlet")
+public class UsuarioServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		Usuario u = obtenerUsuario(request);
+
+		PrintWriter out = response.getWriter();
+		UsuarioDAO usuarioDAO = new UsuarioDAOImpl();
+
+		try {
+			usuarioDAO.registrar(u);
+			out.println("Registrado con éxito");
+		} catch (Exception e) {
+			response.sendError(500);
+		}
+
+	}
+
+	private Usuario obtenerUsuario(HttpServletRequest request) {
+		Usuario u = new Usuario();
+		u.setNombre(request.getParameter("nombre"));
+		u.setApellido(request.getParameter("apellido"));
+		u.setUsuario(request.getParameter("usuario"));
+		u.setContrasena(request.getParameter("contrasena"));
+		u.setPais(request.getParameter("pais"));
+		u.setTecnologia(request.getParameter("tecnologia"));
+		return u;
+	}
+}
